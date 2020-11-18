cd36638064d7f628ad2d8e7215accdc9abc0e65f
Federico Quarta <s280113@studenti.polito.it>
2020-11-18 01:39:55 +0100

    [PULSEBS-45] Fix test for email confirmation

--- test/lecture.js
+++ test/lecture.js
@@ -2,6 +2,8 @@ const app = require("../server/server.js");
 const knex = require("../server/db");
 var request = require("supertest");
 const { expect } = require("chai");
+const moment = require("moment");
+const emailController = require("../server/controllers/email-controller");
 const MailSlurp = require("mailslurp-client").default;
 
 const apiKey = "550e9641efa0f072e78ce2a7485fcd3ceb91011f4165db1de5dede3ccf0ef042";
@@ -43,8 +45,8 @@ const lectureTuple = {
     name: "Lecture 1",
     course: courseTuple.id,
     lecturer: teacherTuple.id,
-    start: "2020-11-19 11:30:00",
-    end: "2020-11-19 14:30:00",
+    start: moment().add(1, "hours").format("YYYY-MM-DD HH:mm:ss"),
+    end: moment().add(2, "hours").format("YYYY-MM-DD HH:mm:ss"),
     capacity: 25,
 };
 
@@ -53,6 +55,11 @@ const courseStudentTuple = {
     student_id: userTuple.id
 };
 
+const lectureBookingTuple = {
+    lecture_id: lectureTuple.id,
+    student_id: userTuple.id,
+    booked_at: moment().subtract(1, "hours").format("YYYY-MM-DD HH:mm:ss")
+};
 
 const authenticatedUser = request.agent(app);
 
@@ -69,13 +76,21 @@ describe("Lecture test", async function() {
         await knex("course").insert(courseTuple);
         await knex("lecture").insert(lectureTuple);
         await knex("course_available_student").insert(courseStudentTuple);
-        const res = await authenticatedUser
-          .post("/api/auth/login")
-          .send(userCredentials)
-          .expect(200);
     });
 
     describe("reservation test", async () => {
+        
+        let inbox;
+
+        before(async () => {
+            const res = await authenticatedUser
+              .post("/api/auth/login")
+              .send(userCredentials)
+              .expect(200);
+            inbox = (await mailslurp.getInboxes())[0];
+            mailslurp.emptyInbox(inbox.id); 
+        });
+
         it("book a seat: should return a 200 response", async () => {
             const res = await authenticatedUser
                 .post(`/api/lectures/${lectureTuple.id}/book`)
@@ -83,8 +98,6 @@ describe("Lecture test", async function() {
         });
 
         it("should receive an email", async() => {
-            const inbox = (await mailslurp.getInboxes())[0];
-            mailslurp.emptyInbox(inbox.id); 
             const email = await mailslurp.waitForLatestEmail(inbox.id);
             expect(email.body).to.match(/You have successfully booked a seat/);
         });
