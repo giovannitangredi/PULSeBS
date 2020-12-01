4a3ee638dd1d6418922efbbf019c9bfe6c8d8c8f
Enrico Carraro <enrico.carraro@studenti.polito.it>
2020-12-01 13:02:12 +0100

    [PULSEBS-68] Fix: hide button when lecture is not convertible to remote.

--- src/components/LectureItem.jsx
+++ src/components/LectureItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import ConfirmDialog from "./ConfirmDialog";
+import moment from "moment";
 // ({lecture: {title, start, end, extendedProps: {status, id}}, handleConvert})
 const LectureItem = ({
   lecture: {
@@ -28,15 +29,18 @@ const LectureItem = ({
             <div className="col-lg-3">
               <label style={{ textTransform: "capitalize" }}>
                 {status === "distance" ? "Remote" : status}{" "}
-                {status != "distance" && (
-                  <ConfirmDialog
-                    body="Are you sure you want to hold this lecture remotely?"
-                    action={() => {
-                      handleConvert(id);
-                    }}
-                    buttonText="Change"
-                  />
-                )}
+                {moment()
+                  .add(30, "minutes")
+                  .isBefore(moment(start, "YYYY-MM-DD HH:mm:ss")) &&
+                  status === "presence" && (
+                    <ConfirmDialog
+                      body="Are you sure you want to hold this lecture remotely?"
+                      action={() => {
+                        handleConvert(id);
+                      }}
+                      buttonText="Change"
+                    />
+                  )}
               </label>
             </div>
           </div>
