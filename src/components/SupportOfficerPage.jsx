e39f8593488725ad8858c80aba1f895ed1ed2f0b
Federico Quarta <s280113@studenti.polito.it>
2020-12-09 23:33:24 +0100

    [PULSEBS-92] removed unused imports

--- src/components/SupportOfficerPage.jsx
+++ src/components/SupportOfficerPage.jsx
@@ -1,13 +1,6 @@
 import React, {useEffect, useState } from "react";
-// import { BookingLectureList } from "./BookingLectureList";
-import { BookedLectureList } from "./BookedLectureList";
-import Container from "react-bootstrap/Container";
 import axios from "axios";
 import { Col, Form, Row, Tab, ListGroup, Button, Spinner } from "react-bootstrap";
-import { CourseDetail } from "./TeacherStatistics";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import moment from "moment";
 import { Check, X } from "react-bootstrap-icons";
 
 
