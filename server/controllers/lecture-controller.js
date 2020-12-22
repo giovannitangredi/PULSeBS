b42f774df2e8632685001e3b122018d2e8adfa17
Federico Quarta <s280113@studenti.polito.it>
2020-12-22 15:15:44 +0100

    [PULSEBS-145] Modify backend for attendances recording Each request will update the status of all the student booked

--- server/controllers/lecture-controller.js
+++ server/controllers/lecture-controller.js
@@ -414,9 +414,9 @@ exports.recordAttendances = async (req, res) => {
   let queries;
 
   try {
-    if(!presentStudents.length) {
+    if(!Array.isArray(presentStudents)) {
       throw {
-        msg: `Invalid body: a not-empty array containing the student ids is expected`,
+        msg: `Invalid body: an array containing the student ids is expected`,
         status: 422,
       };
     }
@@ -452,6 +452,15 @@ exports.recordAttendances = async (req, res) => {
       };
     }
 
+    const bookedStudents = 
+        ( 
+          await knex.select("student_id")
+            .from("lecture_booking")
+            .where("lecture_id", lectureId)
+        ).map(student => student.student_id);
+    
+    const absentStudents = bookedStudents.filter(studentId => !presentStudents.includes(studentId));
+    
     await knex.transaction(trx => {
       // set the status to "present" for all the students in presentStudents...
       queries = 
@@ -464,19 +473,22 @@ exports.recordAttendances = async (req, res) => {
               .transacting(trx)
         );
       
-      // ... and set the status to "absent" for all the other students booked at lectureId, not in presentStudents 
-      // and whose status hasn't yet been set 
+      // ... and set the status to "absent" for all the other students in absentStudents
       queries.push(
-        knex("lecture_booking")
-          .where("lecture_id", lectureId)
-          .whereNull("status")
-          .update({status: "absent"})
-          .transacting(trx)
+        ...absentStudents.map( 
+          studentId => 
+            knex("lecture_booking")
+              .where("lecture_id", lectureId)
+              .andWhere("student_id", studentId)
+              .update({status: "absent"})
+              .transacting(trx)
+        )
       );
 
       Promise.all(queries)
         .then((result) => {
-          if(result.slice(0, -1).includes(0)) {
+          console.log(result);
+          if(result.includes(0)) {
             trx.rollback({
               msg: `There was an error recording the attendances: Wrong ids in the list`,
               status: 400,
